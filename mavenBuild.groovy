eda82ac94a831ce44e255bb039045484f1c20f5b
ganesh <ganshu2022@gmail.com>
2026-06-07 13:21:27 +0530

    Fix Maven build helper to use system Maven when wrapper files are missing

--- mavenBuild.groovy
+++ mavenBuild.groovy
@@ -1,8 +1,12 @@
 def call() {
   sh '''
     cd SpringBootJspApp
-    chmod +x mvnw
-    ./mvnw clean package -DskipTests
+    if [ -x ./mvnw ] && [ -f ./.mvn/wrapper/maven-wrapper.properties ]; then
+      chmod +x mvnw
+      ./mvnw clean package -DskipTests
+    else
+      mvn clean package -DskipTests
+    fi
     mkdir -p ${WORKSPACE}/application_build_output
     cp target/SpringBootJsp.war ${WORKSPACE}/application_build_output/
   '''
